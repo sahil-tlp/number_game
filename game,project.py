1baa55b13ff54b1576fb64e53b8c9b591905dacf
sahilmeena27 <sahilmeena2058@gmail.com>
2023-07-25 14:06:52 +0530

    uncommenting lines

--- game,project.py
+++ game,project.py
@@ -1,23 +1,20 @@
 #create a game
 
-#######################################
-# import random
-# comp=random.randint(1,10)
-# # print(comp)
-# i=1    #not zero bcoz ,range of gussing is (1 to 10)
-# for i in range(0,6):
-#     num=int(input("enter a number\n"))
-#     if i==5:    #i==5 is not works when it put after((((( i=i+1)))))remember.
-#         print("hard luck, you attempt your  all five chances ,the selected",comp,"is the number") 
-#     elif num<comp:
-#         print("sorry, your number is low ,take another try")
-#     elif num>comp:
-#         print("sorry, your number is greater than comp ,take another try")
-# ##    elif i==5:    #i==5 is not works when it put after((((( i=i+1)))))remember.
-#   #      print("hard luck, you attempt your  all five chances ,the random number is",comp)
-#     elif num==comp:
-#         print("congrates,you are winner in",i,"attempts")
-#         break
-    
-########################        
+
+import random
+comp=random.randint(1,10)
+i=1 
+for i in range(0,6):
+    num=int(input("enter a number\n"))
+    if i==5:    
+        print("hard luck, you attempt your  all five chances ,the selected",comp,"is the number") 
+    elif num<comp:
+        print("sorry, your number is low ,take another try")
+    elif num>comp:
+        print("sorry, your number is greater than comp ,take another try")
+
+    elif num==comp:
+        print("congrates,you are winner in",i,"attempts")
+        break
+     
 
